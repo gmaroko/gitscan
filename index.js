c3024c6206d7220065a2d75428ce864c56d19e2d
Jarod Peachey <jwpeachey107@aol.com>
2020-01-21 19:56:48 -0500

    Now getting all issues from repo

--- index.js
+++ index.js
@@ -2,18 +2,42 @@
  * This is the main entrypoint to your Probot app
  * @param {import('probot').Application} app
  */
-module.exports = app => {
-  // Your code here
-  app.log('Yay, the app was loaded!')
 
-  app.on('issues.opened', async context => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    return context.github.issues.createComment(issueComment)
-  })
+module.exports = (app) => {
+  // Your code here
+  app.log('Yay, the app was loaded!');
+
+  app.on('issues.opened', async (context) => {
+    const owner = context.payload.repository.owner.login;
+    const repo = context.payload.repository.name;
+
+    // app.log(context.github.issues.listForRepo({
+    //   owner,
+    //   repo
+    // }));
+
+    context.github.issues
+      .listForRepo({
+        owner,
+        repo,
+      })
+      .then((data) => {
+        app.log(data);
+      })
+      .catch((err) => {
+        app.log('Error: ', err);
+      });
+
+    const issueComment = context.issue({
+      body: 'Thanks for opening this issue!',
+    });
+
+    return context.github.issues.createComment(issueComment);
+  });
 
   // For more information on building apps:
   // https://probot.github.io/docs/
 
   // To get your app running against GitHub, see:
   // https://probot.github.io/docs/development/
-}
+};
